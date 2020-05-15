dbb298dc7bb955d040cca90565b55c44145df96a
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-05-15 08:34:36 -0700

    add minimum amount of code, test one is now passing.

--- lib/Word.rb
+++ lib/Word.rb
@@ -1,4 +1,17 @@
 require ('pry')
 
 class Word
+  attr_reader :id, :name
+  @@words = {}
+  @@total_rows = 0
+
+  def initialize(id, name)
+    @name = name
+    @id = id || @@total_rows += 1
+  end
+
+  def self.all
+    @@words.values()
+  end
+  
 end
