f54b17871af94b47eb73ac5e48942b475782655d
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-05-15 09:00:36 -0700

    add test for clear method, expect to fail.

--- spec/Word_definer_spec.rb
+++ spec/Word_definer_spec.rb
@@ -2,27 +2,46 @@ require ('Word.rb')
 require ('pry')
 require ('rspec')
 
+describe'#Word' do
 
-describe('.all') do
-  it('returns an empty array when there are no words') do
+  before(:each) do
+    Word.clear()
+  end
+  
+  describe('#save') do
+  it('saves a word') do
+    word = Word.new('coffee', nil)
+    word.save()
+    word1 = Word.new('sugar', nil)
+    word1.save()
+    expect(Word.all).to(eq([word, word1]))
+    end
+  end
+  
+  describe('.all') do
+    it('returns an empty array when there are no words') do
+      expect(Word.all).to(eq([]))
+    end
+  end
+
+  describe('.clear') do
+    it('clears all words') do
+      word = Word.new('coffee', nil)
+    word.save()
+    word1 = Word.new('sugar', nil)
+    word1.save()
     expect(Word.all).to(eq([]))
+    end
   end
-end
 
-describe('#save') do
-it('saves a word') do
-  word = Word.new('coffee', nil)
-  word.save()
-  word1 = Word.new('sugar', nil)
-  word1.save()
-  expect(Word.all).to(eq([word, word1]))
-  end
-end
 
-describe('#==') do
-it('overrides the == operator so two words with same attr are consider similar') do
-  word = Word.new('coffee', nil)
-  word = Word.new('coffee', nil)
-  expect(word).to(eq(word))
+  describe('#==') do
+  it('overrides the == operator so two words with same attr are consider similar') do
+    word = Word.new('coffee', nil)
+    word = Word.new('coffee', nil)
+    expect(word).to(eq(word))
+    end
   end
+
+
 end
