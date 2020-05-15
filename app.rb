5b492fc2e6fd460c2422bca6f8524899aff7e8ff
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-05-15 10:42:27 -0700

    add homepage route to app.rb.

--- app.rb
+++ app.rb
@@ -2,3 +2,19 @@ require('sinatra')
 require('sinatra/reloader') 
 require('./lib/Word.rb')
 also_reload('lib/**/*.rb') 
+
+get('/') do
+  @words = Word.all 
+  erb(:words)
+end
+
+get('/homepage') do
+  if (params[:search])
+    @words = Word.search(params[:search])
+  elsif params[:sort]
+    @words = Word.sort
+  else
+    @words = Word.all
+  end
+  erb(:homepage)
+end
