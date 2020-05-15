8d7f70c0680d9cea923517ac9438767699219814
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-05-15 08:40:17 -0700

    add test for save method, expect to fail.

--- spec/Word_definer_spec.rb
+++ spec/Word_definer_spec.rb
@@ -2,6 +2,14 @@ require ('Word.rb')
 require ('pry')
 require ('rspec')
 
+describe('#save') do
+it('saves a word') do
+  word = Word.new('coffee', nil)
+  word.save()
+  word1 = Word.new('sugar')
+  word1.save()
+  expect(Word.all).to(eq([word, word1]))
+
 describe('.all') do
   it('returns an empty array when there are no words') do
    expect(Word.all).to(eq([]))
